f291451bd31491a9ab749a579077e29d978aae58
Hammad Bin Sajjad <hammadbinsajjad2002@gmail.com>
2025-04-09 23:41:09 +0500

    stock processing during ingestion

--- ingestion/sources/mistore.py
+++ ingestion/sources/mistore.py
@@ -7,19 +7,15 @@ STORE_NAME = "mistore"
 
 UNWANTED_ATTRIBUTES = [
     "images", "options", "tags",
-    "product_type", "body_html",
-    "vendor",
+    "body_html", "vendor", "variants",
 ]
-
-UNWANTED_VARIANT_ATTRIBUTES = ["featured_image"]
 
 
 @dlt.source(name="mistore")
 def mistore_scraper():
     processing_steps = [
-        {"map": remove_unwanted_attributes},
-        {"map": remove_unwanted_variant_attributes},
         {"map": add_custom_attributes},
+        {"map": remove_unwanted_attributes},
     ]
 
     config = shopify_config("https://mistore.pk", processing_steps=processing_steps)
@@ -34,14 +30,12 @@ def remove_unwanted_attributes(raw_product):
     return raw_product
 
 
-def remove_unwanted_variant_attributes(raw_product):
-    for attribute in UNWANTED_VARIANT_ATTRIBUTES:
-        for variant in raw_product["variants"]:
-            variant.pop(attribute)
-
-    return raw_product
-
-
 def add_custom_attributes(raw_product):
     raw_product["store_name"] = STORE_NAME
+    raw_product["in_stock"] = is_in_stock(raw_product)
+
     return raw_product
+
+
+def is_in_stock(raw_product):
+    return any(raw_variant["available"] for raw_variant in raw_product["variants"])
