4126c28ac9af0753f0f192bdbc0847bbc4d390a2
Hammad Bin Sajjad <hammadbinsajjad2002@gmail.com>
2025-04-12 19:06:26 +0500

    Fix overriding of secrets.toml private key by env vars

--- ingestion/utils/normalize_env_vars.py
+++ ingestion/utils/normalize_env_vars.py
@@ -12,4 +12,6 @@ def normalize_environment_variables():
 
 def normalize_bigquery_private_key():
     private_key_env_key = "DESTINATION__BIGQUERY__CREDENTIALS__PRIVATE_KEY"
-    os.environ[private_key_env_key] = os.getenv(private_key_env_key, "").replace("\\n", "\n")
+
+    if big_query_private_key := os.getenv(private_key_env_key, ""):
+        os.environ[private_key_env_key] = big_query_private_key.replace("\\n", "\n")
