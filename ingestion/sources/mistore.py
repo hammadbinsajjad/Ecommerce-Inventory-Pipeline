0dffcd12b4d6db583248941cb1d256f23a728b25
Hammad Bin Sajjad <hammadbinsajjad2002@gmail.com>
2025-04-13 22:21:43 +0500

    Add variants table and fix table names

--- ingestion/sources/mistore.py
+++ ingestion/sources/mistore.py
@@ -3,12 +3,14 @@ from dlt.sources.rest_api import rest_api_resources
 
 from ingestion.sources.templates.shopify import shopify_config
 
-STORE_NAME = "mistore"
+STORE_ID = "mistore"
 
 UNWANTED_ATTRIBUTES = [
     "images", "options", "tags",
-    "body_html", "vendor", "variants",
+    "body_html", "vendor",
 ]
+
+UNWANTED_VARIANT_ATTRIBUTES = ["featured_image"]
 
 
 @dlt.source(name="mistore")
@@ -16,6 +18,7 @@ def mistore_scraper():
     processing_steps = [
         {"map": add_custom_attributes},
         {"map": remove_unwanted_attributes},
+        {"map": remove_unwanted_variant_attributes},
     ]
 
     config = shopify_config("https://mistore.pk", processing_steps=processing_steps)
@@ -30,12 +33,14 @@ def remove_unwanted_attributes(raw_product):
     return raw_product
 
 
-def add_custom_attributes(raw_product):
-    raw_product["store_name"] = STORE_NAME
-    raw_product["in_stock"] = is_in_stock(raw_product)
+def remove_unwanted_variant_attributes(raw_product):
+    for attribute in UNWANTED_VARIANT_ATTRIBUTES:
+        for raw_variant in raw_product["variants"]:
+            raw_variant.pop(attribute)
 
     return raw_product
 
 
-def is_in_stock(raw_product):
-    return any(raw_variant["available"] for raw_variant in raw_product["variants"])
+def add_custom_attributes(raw_product):
+    raw_product["store_id"] = STORE_ID
+    return raw_product
